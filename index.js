25417ee069567214ff94e701a3d2cd553be943b2
root <you@example.com>
2023-05-27 11:34:53 +0000

    2nd commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const peopleNames = require("./..country/state/city/index");
-const getFirstNames = require("./..utilities/utils/index");
+const peopleNames = require("../country/state/city/index");
+const getFirstNames = require("../utilities/utils/index");
 
 function getPeopleInCity(peopleNames) {
   return getFirstNames(peopleNames);
 }
 
-console.log(getPeopleInCity);
+module.exports = getPeopleInCity;
